ce075f36099d1414194c200ba8bccfaca7301c2a
Parker Fisher <pfishe13@kent.edu>
2022-10-12 13:08:44 -0400

    Updated routing using express

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
-console.log('Test');
-
-const http = require('http');
-const fs = require('fs');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = http
-  .createServer(function (req, res) {
-    // http header
-    var url = req.url;
-    if (url === '/') {
-      fs.readFile('index.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    } else if (url === '/about') {
-      fs.readFile('about.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    } else if (url === '/contact') {
-      fs.readFile('contact.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    } else {
-      fs.readFile('404.html', function (err, data) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    }
-  })
-  .listen(3000, function () {
-    // The server object listens on port 3000
-    console.log('server start at port 3000');
-  });
+app.get('/', (reg, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/about', (reg, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.get('/contact', (reg, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'));
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '/404.html'));
+});
